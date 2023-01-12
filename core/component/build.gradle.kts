3b95f28e93f642688309919991c8c247fcdf6421
gshcherbakov <gleb.shcherbakov@noveogroup.com>
2023-01-12 14:11:21 +0300

    added common network error screen

--- core/component/build.gradle.kts
+++ core/component/build.gradle.kts
@@ -9,9 +9,11 @@ android {
 
 dependencies {
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.coil.kt.compose)
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.moshi)
     implementation(libs.koin.android)
+    implementation(libs.androidx.compose.material3)
 }
--- core/component/src/main/java/com/noveogroup/moviecatalog/core/component/MovieComponents.kt
+++ core/component/src/main/java/com/noveogroup/moviecatalog/core/component/MovieComponents.kt
@@ -1,12 +1,22 @@
 package com.noveogroup.moviecatalog.core.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 
 @Composable
@@ -33,4 +43,32 @@ fun MoviePosterPlaceholder() {
         ),
         colorFilter = ColorFilter.tint(Color.LightGray)
     )
+}
+
+@Composable
+fun MovieError(
+    error: String,
+    onRefreshClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_network_error), contentDescription = "Something wrong")
+
+        Text(text = error, fontSize = 24.sp)
+
+        Button(onClick = onRefreshClicked, modifier = Modifier.padding(top = 25.dp)) {
+            Text(text = stringResource(id = R.string.movie_list_refresh_button))
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MovieListErrorPreview() {
+    MovieError("something wrong", {})
 }
