703a000da75892b682698dbf46efcd7c7c663255
gshcherbakov <gleb.shcherbakov@noveogroup.com>
2023-01-12 15:16:07 +0300

    lint fixed

--- core/designsystem/src/main/java/com/noveogroup/moviecatalog/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/noveogroup/moviecatalog/core/designsystem/theme/Theme.kt
@@ -1,13 +1,10 @@
 package com.noveogroup.moviecatalog.core.designsystem.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
     primary = PrimaryColor,
